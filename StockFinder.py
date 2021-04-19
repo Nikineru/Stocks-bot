bc8d5ba2612ea5a27c6100d355eeb8daf18b24f1
Nikineru <55121362+Nikineru@users.noreply.github.com>
2021-04-19 09:56:47 +0700

    #Продолжение перестройки

--- StockFinder.py
+++ StockFinder.py
@@ -14,29 +14,31 @@ class Security:
         File.close()
         return Data
 
-class Stock(Security):
-    def __init__(self):
-        super().__init__('stocks.csv')
-
-class Bond(Security):
-    def __init__(self):
-        super().__init__('bonds.csv')
-
-class ETF(Security):
-    def __init__(self):
-        super().__init__('etfs.csv')
-
-
 class DataBaseWorker:
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(DataBaseWorker, cls).__new__(cls)
+        return cls.instance
+
     def __init__(self):
-        self.Stocks = Stock()
-        self.Bonds = Bond()
-        self.Etfs = ETF()
+        self.Securities = [
+            Security('stocks.csv'),
+            Security('bonds.csv'),
+            Security('etfs.csv')]
     
-    def SecurityDataOfType(self, security_type, country, name):
-        pass
+    def FindSecurityByIndex(self, country, ticker, index):
+        if index > -1 and index < len(self.Securities):
+            for stock in self.Securities[index].Data:
+                if stock[0] == country and stock[-1] == ticker:
+                    return stock
+        return None
 
+    def FindSecurityData(self, country, ticker, security_index=-1):
+        if security_index == -1:
+            for index in range(len(self.Securities)):
+                stock = self.FindSecurityByIndex(country, ticker, index)
 
-
-Worker = DataBaseWorker()
-print(Worker.Etfs.Data)
+                if stock != None:
+                    return stock
+        else:
+            return self.FindSecurityByIndex(country, ticker, security_index)
--- Stocks_bot.py
+++ Stocks_bot.py
@@ -1,8 +1,7 @@
-import requests, re, openpyxl
-import datetime, os, shutil
-import ID_founder
-from ID_founder import program_path
-import pickle 
+import requests
+import re
+import gzip
+from io import StringIO
 from time import time
 from bs4 import BeautifulSoup
 
@@ -10,13 +9,11 @@ user = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KH
 session = requests.Session()
 
 
-days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
-
 def GetStockData(Stock_id, search_date):
     params = {
             "curr_id": Stock_id,
-            "smlID": 0,
-            "header": f'Прошлые данные - {ticker}',
+            "smlID": 1159963,
+            "header": 'AAPL Historical Data', #TODO
             'st_date': search_date,
             'end_date': search_date,
             "interval_sec": 'Daily',
@@ -36,168 +33,14 @@ def GetStockData(Stock_id, search_date):
     URL = "https://www.investing.com/instruments/HistoricalDataAjax"
     Period_Data = requests.post(URL, headers=head, data=params)
 
-    soup = BeautifulSoup(Period_Data.content, 'html.parser')
-    StocksQuote = soup.find("td", { "class" : re.compile(r"^(greenFont|redFont)$") })
+    buf = StringIO(str(Period_Data.content))
+    f = gzip.GzipFile(fileobj=buf)
+    f.read()
 
-    return StocksQuote['data-real-value'] if StocksQuote != None else None
+    #soup = BeautifulSoup(Period_Data.content, 'html.parser')
+    #print(Period_Data.content)
+    #StocksQuote = soup.find("td", { "class" : re.compile(r"^(greenFont|redFont)$") })
+    #return StocksQuote['data-real-value'] if StocksQuote != None else None
 
-def GetStockCountry(Ticker):
-    URL = f'https://ru.investing.com/search/?q={Ticker}'
 
-    header = {
-        'user-agent': user
-    }
-
-    response = requests.get(URL, headers=header)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    FirstStock = soup.find('a', class_ = 'js-inner-all-results-quote-item row')
-    if FirstStock != None:
-        URL = 'https://www.investing.com{}'.format(FirstStock['href'])
-    else:
-        print("I can`t frind stock data")
-
-    response = requests.get(URL, headers=header)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    country_data = soup.find_all('a', class_='font-bold text-secondary hover:underline')
-    return country_data[2].text.lower()
-
-def GetAmericanDate(date):
-    return f"{GetStringFromDay(date.month)}/{GetStringFromDay(date.day)}/{GetStringFromDay(date.year)}"
-
-def GetStringFromDay(value):
-    return value if value >= 10 else f"0{value}"
-
-def IsTicker(value:str):
-    value = value.replace(' ', '')
-
-    if re.search(r'[^A-Z_]', value):
-        return False
-    else:
-        return True
-
-def GetInputPosition(sheet, path="Data/InputData.txt"):
-    DatePos = None
-    TickerPos = None
-    path = program_path + path
-
-    if os.path.getsize(path) > 0:
-        with open(path,'rb') as inp:
-            load_data = pickle.load(inp)
-            _date = (0, 0)
-            _ticker = (0, 0)
-
-            if len(load_data) > 0:
-                _date = load_data[0]
-                _ticker = load_data[1]
-                
-                if type(sheet.cell(row=_date[0], column=_date[1]).value) == datetime.datetime:
-                    DatePos = _date
-
-                if IsTicker(str(sheet.cell(row=_ticker[0], column=_ticker[1]).value)):
-                    TickerPos = _ticker
-    else:
-        print("Тут ничего нет ;/")
-
-    if DatePos == None or TickerPos == None:
-        for row in sheet.rows:
-            for cell in row:
-                if TickerPos == None and IsTicker(str(cell.value)):
-                    TickerPos = (cell.row, cell.column)
-
-                if DatePos == None and type(cell.value) == datetime.datetime:
-                    DatePos = (cell.row, cell.column)
-                
-                if TickerPos != None and DatePos != None:
-                    break
-
-        with open(path,'wb') as out:
-            save_data = [DatePos, TickerPos]
-            pickle.dump(save_data,out)
-
-    return (DatePos, TickerPos)
-
-start_time = time()
-path_to_TablePath = (program_path + 'Data/TablePath.txt')
-path = ''
-
-if os.path.getsize(path_to_TablePath) > 0:
-    with open(path_to_TablePath,'rb') as inp:
-            path = pickle.load(inp)
-if len(path) < 1:
-    path = input("Введите путь до таблицы: ")
-    path = path.replace('\"', '')
-    path = path.replace('\\', '/')
-
-    if len(path) > 0:
-        with open(path_to_TablePath,'wb') as out:
-                pickle.dump(path, out)
-try:
-    reserve_path = path.split('/')
-    reserve_path = '/'.join(reserve_path[:len(reserve_path) - 1]) + '/reserve_table.xlsx'
-    shutil.copyfile(path, reserve_path)
-
-    book = openpyxl.load_workbook(path) 
-    sheet = book.active
-except:
-    f = open(path_to_TablePath, 'w')
-    f.close()
-    wait = input('Нажмите Enter, для продолжения')
-    exit('Введите корректный путь')
-
-Tickers = list()
-RowOfLastTicker = 1
-
-Input = GetInputPosition(sheet)
-for row in sheet.iter_rows(min_row=Input[1][0], min_col=1):
-    for cell in row:
-        value = str(cell.value)
-        if IsTicker(value):
-            country = sheet.cell(row=cell.row, column=Input[1][1] + 1)
-            Tickers.append((cell, country))
-            RowOfLastTicker += 1
-
-Date = sheet.cell(row=Input[0][0], column=Input[0][1]).value
-DayOfWeek = Date.weekday()
-
-if DayOfWeek > 4:
-    print(f"В {days[Date.weekday()]} торги не ведутся, введите другой день!")
-    wait = input('Нажмите Enter, для продолжения')
-    exit()
-
-StartDate = GetAmericanDate(Date)
-if Date > datetime.datetime.now():
-    print("Введите корректную дату")
-    wait = input('Нажмите Enter, для продолжения')
-    exit()
-
-EndDate = StartDate
-print(f"Я буду работать с данными тикерами: {', '.join([i[0].value for i in Tickers])}")
-print(f"Данные будут получены на: {Date}\n")
-
-for ticker_data in Tickers:
-    ticker = ticker_data[0]
-    country = ticker_data[1]
-    if country.value == None:
-        country.value = GetStockCountry(ticker.value)
-    tick_val = ticker.value
-
-    if '_P' in tick_val and tick_val.index('P') == len(tick_val) - 1:
-        tick_val = tick_val[:len(tick_val) - 1] + 'p'
-    
-    StockID = ID_founder.GetStockData(ticker=tick_val, country=country.value)
-    Quote = GetStockData(StockID, StartDate)
-
-    if Quote == None:
-        Quote = '-'
-
-    sheet.cell(row=ticker.row, column=ticker.column + 2).value = Quote
-    print(f"Я получил акцию с тикером {tick_val}, её котировка - {Quote}")
-    
-try:
-    book.save(path)
-    print('Я выполнил свою работу за - {:0.2f} секунд!'.format(time() - start_time))
-    os.startfile(path)
-    #os.system(f"open {path}")
-except:
-    print("Закройте numbers и перезапустите программу")
+GetStockData(6408, '03/19/2021')
