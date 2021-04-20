629f9b0b701f715fd3425e2e6d1116fd8e8916ec
Nikineru <55121362+Nikineru@users.noreply.github.com>
2021-04-20 12:31:13 +0700

    #Fix this fucking error

--- Stocks_bot.py
+++ Stocks_bot.py
@@ -1,46 +1,44 @@
 import requests
-import re
-import gzip
-from io import StringIO
-from time import time
-from bs4 import BeautifulSoup
+import fake_useragent
 
-user = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1"
-session = requests.Session()
+from lxml.html import fromstring
+from random import randint
 
-
-def GetStockData(Stock_id, search_date):
+def GetStockPrice(Stock_id: int, search_date: str):
     params = {
-            "curr_id": Stock_id,
-            "smlID": 1159963,
-            "header": 'AAPL Historical Data', #TODO
-            'st_date': search_date,
-            'end_date': search_date,
-            "interval_sec": 'Daily',
-            "sort_col": "date",
-            "sort_ord": "DESC",
-            "action": "historical_data"
-        }
+        "curr_id": Stock_id,
+        "smlID": str(randint(1000000, 99999999)),
+        "header": 'AAPL Historical Data',
+        "st_date": search_date,
+        "end_date": search_date,
+        "interval_sec": 'Daily',
+        "sort_col": "date",
+        "sort_ord": "DESC",
+        "action": "historical_data"
+    }
 
     head = {
-        "User-Agent": user,
+        "User-Agent": fake_useragent.UserAgent().random,
         "X-Requested-With": "XMLHttpRequest",
         "Accept": "text/html",
-        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Encoding": "gzip, deflate",
         "Connection": "keep-alive",
     }
 
     URL = "https://www.investing.com/instruments/HistoricalDataAjax"
     Period_Data = requests.post(URL, headers=head, data=params)
 
-    buf = StringIO(str(Period_Data.content))
-    f = gzip.GzipFile(fileobj=buf)
-    f.read()
+    if Period_Data.status_code != 200:
+        print(f"Не удалось подключиться, код - {Period_Data.status_code}")
+        return -1
+    
+    root_ = fromstring(Period_Data.text)
+    path_ = root_.xpath(".//table[@id='curr_table']/tbody/tr")
 
-    #soup = BeautifulSoup(Period_Data.content, 'html.parser')
-    #print(Period_Data.content)
-    #StocksQuote = soup.find("td", { "class" : re.compile(r"^(greenFont|redFont)$") })
-    #return StocksQuote['data-real-value'] if StocksQuote != None else None
+    if path_[0].xpath(".//td")[0].text_content() == 'No results found':
+        print("Информаци не найдена...")
+        return -1
 
+    return float(path_[0].xpath(".//td")[1].get('data-real-value'))
 
-GetStockData(6408, '03/19/2021')
+print(GetStockPrice(6408, '03/23/2021'))
